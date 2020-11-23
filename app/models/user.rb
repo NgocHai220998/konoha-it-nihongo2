43bcf9269d64c7ad5d658e0e050fb95962d69ab4
thanhan1181999 <nguyenthanhan1181999@gmail.com>
2020-11-23 15:42:54 +0700

    reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,55 @@ class User < ApplicationRecord
   has_many :comments
   # see more about enum: 
   # https://edgeapi.rubyonrails.org/classes/ActiveRecord/Enum.html
-  attr_accessor :password_confirmation
+  attr_accessor :password_confirmation, :reset_token
   before_save { self.mail = mail.downcase }
   validates :name, presence: true, length: { maximum: Settings.model.validates.name.maximum }
   validates :mail, presence: true,
-                    length: { maximum: Settings.model.validates.mail.maximum },
-                    format: { with: Settings.model.validates.mail.valid_email_regex },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: Settings.model.validates.password.minimum }, allow_blank: true, confirmation: true
+    length: { maximum: Settings.model.validates.mail.maximum },
+    format: { with: Settings.model.validates.mail.valid_email_regex },
+    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, 
+    length: { minimum: Settings.model.validates.password.minimum },
+    allow_blank: true, confirmation: true
+    
   def authenticate(login_password)
     if login_password == password
       return true
     else
       return false
     end
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ?
+    BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end 
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+   # Returns true if a password reset has expired.
+  def password_reset_expired? 
+    reset_sent_at < 2.hours.ago
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /ja|vi/ do
     root "static_pages#home"
     resources :users, only: [:index, :create, :edit, :update, :delete]
+    resources :password_resets, only:[:new, :create, :edit, :update]
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     get "/login", to: "sessions#new"
